4ed5790dec203b4bad0d58f43bf1f708bd0c17ff
assad93 <samir_caverna@hotmail.com>
2022-06-11 14:47:05 -0300

    normalize api response

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,14 +6,33 @@ import stormGlassNormalizedResponse from "@test/fixtures/stormglass_response_nor
 jest.mock("axios");
 
 describe("StormGlass client", () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it("should return the normalized forecast from the StormGlass service", async () => {
     const lat = -33.145767;
     const lng = 151.123112;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassResponse);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassResponse });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalizedResponse);
+  });
+
+  it("should exclude incomplete data points", async () => {
+    const lat = -33.145767;
+    const lng = 151.123112;
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: "2020-04-26T00:00:00+00:00",
+        },
+      ],
+    };
+
+    const stormGlass = new StormGlass(axios);
   });
 });
