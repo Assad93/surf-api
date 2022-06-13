23facf127e799c840adbdc1be3efa52654b633ae
assad93 <samir_caverna@hotmail.com>
2022-06-13 06:23:35 -0300

    error handling

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -14,7 +14,7 @@ describe("StormGlass client", () => {
 
     mockedAxios.get.mockResolvedValue({ data: stormGlassResponse });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalizedResponse);
   });
@@ -33,6 +33,41 @@ describe("StormGlass client", () => {
       ],
     };
 
-    const stormGlass = new StormGlass(axios);
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual([]);
+  });
+
+  it("should get a generic error from StormGlass service when the request fail before reaching the service", async () => {
+    const lat = -33.145767;
+    const lng = 151.123112;
+
+    mockedAxios.get.mockRejectedValue({ message: "Network Error" });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      "Unexpected error when trying to communicate to StormGlass: Network Error"
+    );
+  });
+
+  it("should get a StormGlassResponseError when the StormGlass service responds with error", async () => {
+    const lat = -33.145767;
+    const lng = 151.123112;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ["Rate Limit reached"] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
   });
 });
